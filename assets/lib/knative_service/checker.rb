96d12de4225ae67fe020b92d1c90c4dd3eda14ba
Jacques Chester <jchester@pivotal.io>
2018-08-02 10:46:39 -0400

    Extract *-er classes from scripts
    These are probably broken, as I don't have a meaningful test script yet.

---
+++ assets/lib/knative_service/checker.rb
@@ -0,0 +1,35 @@
+
+require_relative 'common'
+
+require 'json'
+
+class KnativeServiceChecker
+  def run(raw_input)
+    source = Source.from_input(raw_input)
+    client = KnativeClient.new(source)
+
+    if first_time_checked?(raw_input)
+      puts [client.observed_generation].to_json
+    else
+      latest_version_in_concourse = Version.from_input(raw_input)
+      latest_version_in_knative = client.observed_generation
+
+      if latest_version_in_knative > latest_version_in_concourse
+        previously_unseen_generations = client.all_versions.select { |ver|
+          ver <= latest_version_in_knative && ver > latest_version_in_concourse
+        }
+
+        puts previously_unseen_generations.to_json
+      else
+        puts [latest_version_in_concourse].to_json
+      end
+    end
+  end
+
+  private
+
+  def first_time_checked?(raw_input)
+    parsed_input = JSON.parse(raw_input)
+    !parsed_input.has_key?('version') || parsed_input['version'].nil?
+  end
+end
--- assets/out.rb
+++ assets/out.rb
@@ -1,38 +1,7 @@
 #!/usr/bin/env ruby
 
-require_relative 'lib/common'
+require_relative 'lib/knative_service/putter'
 
 require 'json'
-
-class KnativeServicePutter
-  def run(input_directory:, raw_input:)
-    source = Source.from_input(raw_input)
-    params = Params.from_input(input_directory, raw_input)
-    client = KnativeClient.new(source)
-
-    patch = [
-        {
-            op: 'replace',
-            path: '/spec/runLatest/configuration/revisionTemplate/spec/container/image',
-            value: "#{params.image_repository}@#{params.image_digest}"
-        },
-        {
-            op: 'replace',
-            path: '/spec/runLatest/configuration/revisionTemplate/metadata/annotations/concourseBuild',
-            value: ConcourseBuildUrlHelper::url
-        }
-    ]
-
-    client.patch_service(name: source.name, patch: patch)
-
-    version = client.observed_generation
-    raw_configuration = client.get_configuration_at_generation(version.configuration_generation)
-    metadata = VersionMetadata.from_input(raw_configuration)
-
-    put_output = {version: version, metadata: metadata}.to_json
-
-    puts put_output
-  end
-end
 
 KnativeServicePutter.new.run(input_directory: ARGV[0], raw_input: STDIN.read)
