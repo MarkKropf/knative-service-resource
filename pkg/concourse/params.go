c0f58c8118c92c75036719cc2c90337d42cff935
Jacques Chester <jchester@pivotal.io>
2018-08-17 10:25:31 -0400

    Remove unused *FromSource methods

--- pkg/concourse/params.go
+++ pkg/concourse/params.go
@@ -1,19 +1,6 @@
 package concourse
 
-import "encoding/json"
-
 type PutParams struct {
 	ImageRepository string `json:"image_repository"`
 	ImageDigest     string `json:"image_digest"`
-}
-
-func ParamsFromInput(rawJson string) (*PutParams, error) {
-	params := &PutParams{}
-
-	err := json.Unmarshal([]byte(rawJson), params)
-	if err != nil {
-		return nil, err
-	}
-
-	return params, nil
 }
--- pkg/concourse/source.go
+++ pkg/concourse/source.go
@@ -1,23 +1,8 @@
 package concourse
 
-import (
-	"encoding/json"
-)
-
 type Source struct {
 	Name            string `json:"name"`
 	KubernetesUri   string `json:"kubernetes_uri"`
 	KubernetesToken string `json:"kubernetes_token"`
 	KubernetesCa    string `json:"kubernetes_ca"`
-}
-
-func SourceFromInput(rawJson string) (*Source, error) {
-	newSource := &Source{}
-
-	err := json.Unmarshal([]byte(rawJson), newSource)
-	if err != nil {
-		return nil, err
-	}
-
-	return newSource, nil
 }
